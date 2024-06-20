5d8cd3d3653cc23b5d1e9e7b6d739a1ca331a383
LucianoAbreu97 <142359382+LucianoAbreu97@users.noreply.github.com>
2024-06-19 22:51:16 -0300

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -3,14 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, collection, doc, setDoc, getDoc, getDocs, onSnapshot, addDoc, deleteDoc} from "firebase/firestore";
 
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSxcNm4",
-  authDomain: "easy-pp.com",
-  projectId: "easy73bd5",
-  storageBucket: "easy-dpspot.com",
-  messagingSenderId: "962403",
-  appId: "1:96595c75339d6"
+  apiKey: "AIzaSyAHuMy6e5gMjdeMHUJqXISmz8d--OxcNm4",
+  authDomain: "easy-data-manager-73bd5.firebaseapp.com",
+  projectId: "easy-data-manager-73bd5",
+  storageBucket: "easy-data-manager-73bd5.appspot.com",
+  messagingSenderId: "96595762403",
+  appId: "1:96595762403:web:0b33e93f753705c75339d6"
 };
 
 // Initialize Firebase
